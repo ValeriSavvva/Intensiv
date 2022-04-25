ee52e7382b932ca3e6a8651a4c98a7812adf76c6
MazokGood <Aslan2334@yandex.ru>
2022-04-25 16:06:23 +0300

    Кнопочька Reload

---
+++ TestIntensiv/Assets/Scripts/ReloadGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadGame : MonoBehaviour
+{
+    public void reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Places.areaForShark = false;
+        Places.areaForTable = false;
+    }
+}
